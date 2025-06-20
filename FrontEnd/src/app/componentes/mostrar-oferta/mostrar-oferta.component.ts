6ab50c97369f672d0a430ba01bae750a7e9882f2
Javimix5 <javiusa2018@gmail.com>
2025-06-20 13:41:02 +0200

    change in frontend for home page

--- FrontEnd/src/app/componentes/mostrar-oferta/mostrar-oferta.component.ts
+++ FrontEnd/src/app/componentes/mostrar-oferta/mostrar-oferta.component.ts
@@ -7,19 +7,13 @@ import { OfertasService } from '../../services/ofertas-service.service';
   styleUrls: ['./mostrar-oferta.component.css']
 })
 export class MostrarOfertaComponent implements OnInit {
-offerList: any[] = [];
+offerActivas: any[] = [];
 
   constructor(private ofertasService: OfertasService) {}
 
   ngOnInit(): void {
-    this.ofertasService.getOfferById().subscribe({
-      next: (data: any[]) => {
-        // Aquí filtro solo las activas para mostrar a candidatos
-        this.offerList = data.filter(oferta => oferta.active);
-      },
-      error: (error) => {
-        console.error('Error al cargar ofertas', error);
-      }
+    this.ofertasService.getAllActiveOffers().subscribe(ofertas =>{
+        this.offerActivas = ofertas;  
     });
   }
 }
