670d4260e8c17321e201fff5274ba280eed589a6
Javimix5 <javiusa2018@gmail.com>
2025-06-30 09:39:26 +0200

    Changes Inscriptions

--- src/main/java/com/campusdual/bfp/api/IInscriptionsService.java
+++ src/main/java/com/campusdual/bfp/api/IInscriptionsService.java
@@ -5,6 +5,8 @@ import com.campusdual.bfp.model.dto.InscriptionsDTO;
 import java.util.List;
 
 public interface IInscriptionsService {
+    InscriptionsDTO queryInscriptions(InscriptionsDTO inscriptionsDTO);
+
     //CRUD Operations
     InscriptionsDTO insertInscriptions(InscriptionsDTO inscriptionDTO);
     List<InscriptionsDTO> queryAllInscriptions();
