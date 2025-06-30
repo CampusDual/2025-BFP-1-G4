6f5a98bf806ced006e5f23ade6e6975915925af7
Aldair-GL <alejandro555azpeitia@gmail.com>
2025-06-30 13:47:12 +0200

    Panel de Admin Correjido

--- FrontEnd/src/app/componentes/lista-empresas/lista-empresas.component.ts
+++ FrontEnd/src/app/componentes/lista-empresas/lista-empresas.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { EnterpriseService } from 'src/app/services/enterprise.service';
 
+
 @Component({
   selector: 'app-lista-empresas',
   templateUrl: './lista-empresas.component.html',
@@ -9,8 +10,7 @@ import { EnterpriseService } from 'src/app/services/enterprise.service';
 export class ListaEmpresasComponent {
   enterprisesList: any[] = [];
 
-  constructor(private enterpriseService: EnterpriseService) {}
-
+  constructor(private enterpriseService: EnterpriseService) { }
   ngOnInit(): void {
     this.enterpriseService.getAllEnterprises().subscribe({
       next: (data) => {
@@ -22,18 +22,5 @@ export class ListaEmpresasComponent {
     });
   }
 
-  agregarFila() {
-    this.enterprisesList.push({
-      id: '—',
-      name: '',
-      address: '',
-      email: '',
-      phonenumber: ''
-    });
-  }
-
-  eliminarFila(empresa: any) {
-    const index = this.enterprisesList.indexOf(empresa);
-    if (index > -1) this.enterprisesList.splice(index, 1);
-  }
+  
 }
